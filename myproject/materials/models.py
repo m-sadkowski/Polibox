a969e7d79facd6a56b41f935b3b42828fd7090e9
m-sadkowski <msadkowski000@gmail.com>
2024-07-25 13:52:00 +0200

    Material' icons

--- myproject/materials/models.py
+++ myproject/materials/models.py
@@ -1,9 +1,12 @@
 # materials/models.py
 from django.db import models
 from django.utils.text import slugify
+import os
+
 
 def upload_path(instance, filename):
     return f"materials/{instance.material.slug}/{instance.category}/{filename}"
+
 
 class Material(models.Model):
     title = models.CharField(max_length=200)
@@ -28,6 +31,7 @@ class Material(models.Model):
             self.slug = unique_slug
         super().save(*args, **kwargs)
 
+
 class File(models.Model):
     CATEGORY_CHOICES = [
         ('lectures', 'Lectures'),
@@ -41,3 +45,15 @@ class File(models.Model):
 
     def __str__(self):
         return f"{self.material.title} - {self.category} - {self.file.name}"
+
+    def get_thumbnail_url(self):
+        extension = os.path.splitext(self.file.name)[1].lower()
+        if extension in ['.jpg', '.jpeg', '.png', '.gif']:
+            return self.file.url  # Dla obrazów zwracamy URL pliku
+        elif extension == '.pdf':
+            return '/static/icons/pdf-icon.png'  # Ścieżka do ikony PDF
+        else:
+            return '/static/icons/default-icon.png'  # Ścieżka do domyślnej ikony
+
+    def get_filename(self):
+        return os.path.basename(self.file.name)
